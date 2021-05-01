3af06f49158cec25a309f3830972fee87c3f860d
Shantnu25 <47518138+Shantnu25@users.noreply.github.com>
2021-05-01 11:40:36 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,4 @@
-# --------------
 from csv import reader
-
 
 def explore_data(dataset, start, end, rows_and_columns=False):
     """Explore the elements of a list.
@@ -16,10 +14,6 @@ def explore_data(dataset, start, end, rows_and_columns=False):
     print(dataset[start:end])
     
     
-        
-     
-
-
 def duplicate_and_unique_movies(dataset, index_):
     """Check the duplicate and unique entries.
     
@@ -37,8 +31,6 @@ def duplicate_and_unique_movies(dataset, index_):
     print(len(name)-len(name2))
     
     
-
-
 def movies_lang(dataset, index_, lang_):
     """Extract the movies of a particular language.
     
@@ -57,8 +49,6 @@ def movies_lang(dataset, index_, lang_):
     movies_=[i[:] for i in dataset if i[index_]==lang_]
     return movies_
     
-
-
 
 def rate_bucket(dataset, rate_low, rate_high):
     """Extract the movies within the specified ratings.
@@ -79,75 +69,64 @@ def rate_bucket(dataset, rate_low, rate_high):
     return rated_movies
 
 
-
-
 # Read the data file and store it as a list 'movies'
 opened_file = open(path, encoding="utf8")
 read_file = reader(opened_file)
 movies = list(read_file)
 
 
-# The first row is header. Extract and store it in 'movies_header'.
+#1 The first row is header. Extract and store it in 'movies_header'.
 movies_header=movies[0]
 #print(movies_header)
-print('--------------/n')
-# Subset the movies dataset such that the header is removed from the list and store it back in movies
+
+
+#2 Subset the movies dataset such that the header is removed from the list and store it back in movies
 movies=movies[1:]
 #print(movies[0])
-print('--------------/n')
 
-# Delete wrong data
+
+#3 Delete wrong data
 
 # Explore the row #4553. You will see that as apart from the id, description, status and title, no other information is available.
 # Hence drop this row.
 print(movies[4553])
-print('--------------/n')
 del movies[4553]
 
 
-# Using explore_data() with appropriate parameters, view the details of the first 5 movies.
+#4 Using explore_data() with appropriate parameters, view the details of the first 5 movies.
 explore_data(movies,0,5)
-print('--------------/n')
 
 
-
-# Our dataset might have more than one entry for a movie. Call duplicate_and_unique_movies() with index of the name to check the same.
+#5 Our dataset might have more than one entry for a movie. Call duplicate_and_unique_movies() with index of the name to check the same.
 
 duplicate_and_unique_movies(movies,13)
-print('--------------/n')
 
 
-# We saw that there are 3 movies for which the there are multiple entries. 
+#6 We saw that there are 3 movies for which the there are multiple entries. 
+
 # Create a dictionary, 'reviews_max' that will have the name of the movie as key, and the maximum number of reviews as values.
 name=[i[13] for i in movies]
 print(name[0:5])
-print('--------------/n')
 print(len(name))
-print('--------------/n')
 
 rev=[i[12] for i in movies]
 print(rev[0:5])
-print('--------------/n')
 
 review_max=dict(zip(name,rev))
 #print(review_max)
-print('--------------/n')
 
 
-# Create a list 'movies_clean', which will filter out the duplicate movies and contain the rows with maximum number of reviews for duplicate movies, as stored in 'review_max'. 
+#7 Create a list 'movies_clean', which will filter out the duplicate movies and contain the rows with maximum number of reviews for duplicate movies, as stored in 'review_max'. 
 movies_clean=[*review_max.keys()]
 #print(movies_clean)
-print('--------------/n')
 print(len(movies_clean))
 
-# Calling movies_lang(), extract all the english movies and store it in movies_en.
+
+#8 Calling movies_lang(), extract all the english movies and store it in movies_en.
 movies_en=movies_lang(movies,3,'en')
 #print(movies_en[0:2])
-print('###############--------------/n')
 
-# Call the rate_bucket function to see the movies with rating higher than 8.
+
+#9 Call the rate_bucket function to see the movies with rating higher than 8.
 high_rated_movies=rate_bucket(movies_en,8.0,11)
 print(len(high_rated_movies))
-
-
-
